d3a83cca49caad221d0a6dcaed50d191e2bae7a9
YachWise <jacobhwiseios@gmail.com>
2022-11-27 21:19:23 -0600

    first commit for catio

--- Cat.io/GameScene.swift
+++ Cat.io/GameScene.swift
@@ -1,88 +1,161 @@
-//
-//  GameScene.swift
-//  Cat.io
-//
-//  Created by Jacob Wise on 11/27/22.
-//
-
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+class GameScene: SKScene
+{
+    var touchBall: Bool = false
+    var innerKnob: SKShapeNode!
+    var outerKnob: SKShapeNode!
+    var cameraNode = SKCameraNode()
+    var player: SKSpriteNode!
+    var ground = SKSpriteNode()
     
     override func didMove(to view: SKView) {
+        //self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        self.createJoyStick()
+        self.createPlayer()
+    }
+    
+    func createPlayer()
+    {
+        self.player = SKSpriteNode(texture: SKTexture(imageNamed: "cat1_idle_00"))
+        self.player.size = CGSize(width: 150, height: 150)
+        self.addChild(player)
+        self.addChild(cameraNode)
+        self.createPlayerIdle()
+        camera = self.cameraNode
+    }
+    
+    func createPlayerRun()
+    {
+        var runTexture: [SKTexture] = []
+        let atlas = SKTextureAtlas(named: "cat1_run")
+        let atlasCount = atlas.textureNames.count - 1
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        for i in 0...atlasCount
+        {
+            let leadingZero = (String(format: "%02d", i))
+            let texture = SKTexture(imageNamed: "cat1_run_\(leadingZero)")
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            runTexture.append(texture)
         }
+        let run = SKAction.animate(with: runTexture, timePerFrame: 0.09)
+        let repeated = SKAction.repeatForever(run)
+        self.player.run(repeated)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+    func createPlayerIdle()
+    {
+        var idleTexture: [SKTexture] = []
+        let atlas = SKTextureAtlas(named: "cat1_idle")
+        let atlasCount = atlas.textureNames.count - 1
+        
+        for i in 0...atlasCount
+        {
+            let leadingZero = (String(format: "%02d", i))
+            let texture = SKTexture(imageNamed: "cat1_idle_\(leadingZero)")
+            
+            idleTexture.append(texture)
         }
+        let idle = SKAction.animate(with: idleTexture, timePerFrame: 0.09)
+        let repeated = SKAction.repeatForever(idle)
+        self.player.run(repeated)
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+    func createJoyStick()
+    {
+        //inner knob setup
+        self.innerKnob = SKShapeNode(circleOfRadius: 50)
+        self.innerKnob.name = "ball"
+        self.innerKnob.zPosition = 2
+        self.innerKnob.position = CGPoint(x: self.frame.midX, y: -self.size.height*0.3)
+        self.innerKnob.fillColor = .white
+        self.innerKnob.strokeColor = .white
+        
+        //outer knob setup
+        self.outerKnob = SKShapeNode(circleOfRadius: 100)
+        self.outerKnob.fillColor = .clear
+        self.outerKnob.strokeColor = .white
+        self.outerKnob.position = self.innerKnob.position
+    
+        //add both knobs to camera
+        self.cameraNode.addChild(self.innerKnob)
+        self.cameraNode.addChild(self.outerKnob)
+        
     }
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+    override func update(_ currentTime: TimeInterval) {
+        if (self.touchBall){
+            let dx = self.innerKnob.position.x - self.outerKnob.position.x
+            let dy = self.innerKnob.position.y - self.outerKnob.position.y
+            let angle = atan2(dy, dx)
+            self.player.position = CGPoint(x:cos(angle)*5+self.player.position.x, y: sin(angle)*5+self.player.position.y)
+            
+            self.cameraNode.position = self.player.position
+            let x = player.position.x
+            let y = player.position.y
+            let playerPos = CGPoint(x: x, y: y)
+            //print(playerPos)
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        let touch = touches.first
+        let location = touch!.location(in: self)
+        if atPoint(location).name == "ball"
+        {
+            self.player.removeAllActions()
+            self.createPlayerRun()
+            print("ball touched")
+            self.touchBall = true
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        let touch = touches.first
+        let location = touch!.location(in: self)
+        let convertLocation = self.convert(location, to: self.cameraNode)
+        
+        if (self.touchBall)
+        {
+            let dx = convertLocation.x - outerKnob.position.x
+            let dy = convertLocation.y - outerKnob.position.y
+            let angle = atan2(dy,dx)
+            
+            self.innerKnob.position = convertLocation
+            let square = sqrt(dx*dx + dy*dy)
+            
+            //face the player right side
+            if (self.innerKnob.position.x > outerKnob.position.x)
+            {
+                self.player.xScale = 1
+            }
+            //turn player left side
+            else if (self.innerKnob.position.x < self.outerKnob.position.x)
+            {
+                self.player.xScale = -1
+            }
+            if (square >= 100)
+            {
+                self.innerKnob.position = CGPoint(x: cos(angle)*100 + self.outerKnob.position.x, y: sin(angle)*100 + self.outerKnob.position.y)
+            }
+            else
+            {
+                self.innerKnob.position = convertLocation
+            }
+            
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        self.innerKnob.position = self.outerKnob.position
+        
+        self.touchBall = false
+        self.player.removeAllActions()
+        self.createPlayerIdle()
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
     }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+
 }
